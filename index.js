3b8c771d0c1e039b8be1c76f1aae6bbb6cafc077
ikhwansiskom14 <dewa.siskom@gmail.com>
2020-09-21 18:43:58 +0700

    first commit ' :

---
+++ index.js
@@ -0,0 +1,126 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
+
+const app = express()
+
+// conf db connection
+const conn = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'monitoring_pekerjaan'
+})
+
+// create connection to db
+conn.connect((err) => {
+      if (err) throw err
+      console.log('mysql connect...')
+})
+
+// parse req content-type : application/JSON
+app.use(bodyParser.json())
+// parser req content-type : x-www-form-urlcode
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
+app.get('/test', (req, res) => {
+      res.json({
+            message: "welcome to express monitoring pekerjaan"
+      })
+})
+
+app.get('/activities', (req, res) => {
+      let sql = 'SELECT * FROM activities'
+      let query = conn.query(sql, (err, result) => {
+            if (err) throw err
+            res.status(200).send({
+                  message: 'success',
+                  data: result
+            })
+      })
+});
+
+app.get('/activity/:id', (req, res) => {
+      const reqId = req.params.id
+      let sql = `SELECT * FROM activities WHERE id=${reqId}`
+      let query = conn.query(sql, (err, result) => {
+            if (err) throw err
+            res.status(200).send({
+                  message: 'success',
+                  data: result[0]
+            })
+      })
+});
+
+app.post('/activity', (req, res) => {
+      const reqTitle = req.body.title
+      const reqDesc = req.body.description
+      const reqUserId = req.body.user_id
+
+      if (!reqTitle || !reqDesc || !reqUserId) {
+            return res.status(422).send({ 
+                  errors: {
+                        message: "Title, Description Required",
+                  }
+            });
+      }
+
+      // return res.send(sampleFile.mimetype)
+      if (!req.files || Object.keys(req.files).length === 0) {
+            return res.status(422).send({ 
+                  errors: {
+                  message: "Harus melampirkan foto",
+            }});
+      }
+
+      const data = { title : reqTitle, description : reqDesc, user_id : reqUserId }
+      const sql = `INSERT INTO activities SET ?`
+      
+      let query = conn.query(sql, data, (err, result) => {
+            if (err) throw err
+            res.status(200).send({
+                  message: 'success',
+            })
+      })
+});
+
+app.post('/activity/update/:id', (req, res) => {
+      const paramsId = req.params.id
+      const reqTitle = req.body.title
+      const reqDesc = req.body.description
+      const reqUserId = req.body.user_id
+
+      let sql = `UPDATE activities SET title='${reqTitle}', description='${reqDesc}', user_id=${reqUserId} WHERE id=${paramsId}`
+      let query = conn.query(sql, (err, result) => {
+            if (err) throw err
+            res.status(200).send({
+                  message: 'success'
+            });
+      })
+});
+
+app.delete('/activity/:id', (req, res) => {
+      const paramsId = req.params.id
+      try {
+            let sql = `DELETE FROM activities WHERE id=${paramsId}`
+            let query = conn.query(sql, (err, result) => {
+                  if (err) throw err
+                  if (result) {
+                        res.status(200).send({
+                              message: 'success',
+                              data: result
+                        });
+                  }
+            })
+      } catch (error) {
+            res.status(500).send({
+                  message: error.message || 'error delete data'
+            });
+      }
+})
+
+const server = app.listen(5000, () => {
+      console.log('server running on port 5000 ..')
+})
